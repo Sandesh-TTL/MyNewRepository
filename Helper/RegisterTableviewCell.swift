9711cefafce6830ac3306a3404e168aa12deeb9b
Sandesh <sandesh.gujar@thyrocare.com>
2020-01-28 17:49:24 +0530

    FST dev branch commit

---
+++ Helper/RegisterTableviewCell.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterTableviewCell.swift
+//  FST
+//
+//  Created by Nityanand on 28/01/20.
+//  Copyright © 2020 Nityanand. All rights reserved.
+//
+
+import Foundation
---
+++ View/BookMovieTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookMovieTableCell.swift
+//  FST
+//
+//  Created by Nityanand on 28/01/20.
+//  Copyright © 2020 Nityanand. All rights reserved.
+//
+
+import UIKit
+
+class BookMovieTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
