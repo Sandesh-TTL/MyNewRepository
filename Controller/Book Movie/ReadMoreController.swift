4fd0a004b879e7562d00548d29fca02dad1b29f4
Sandesh <sandesh.gujar@thyrocare.com>
2020-02-01 11:48:04 +0530

    udating Book My show screen

---
+++ Controller/Book Movie/ReadMoreController.swift
@@ -0,0 +1,30 @@
+//
+//  ReadMoreController.swift
+//  FST
+//
+//  Created by Nityanand on 30/01/20.
+//  Copyright © 2020 Nityanand. All rights reserved.
+//
+
+import UIKit
+
+class ReadMoreController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Extension/UIToolbarExt.swift
@@ -0,0 +1,9 @@
+//
+//  UIToolbarExt.swift
+//  FST
+//
+//  Created by Nityanand on 30/01/20.
+//  Copyright © 2020 Nityanand. All rights reserved.
+//
+
+import Foundation
